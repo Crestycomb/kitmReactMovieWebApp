f520b8a33125458d324c164f550f87dec8cfd4a2
Crestycomb <ignas.klydzia@gmail.com>
2020-01-12 15:46:42 +0200

    first commit

---
+++ src/components/app/app.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import styles from './app.css'
+
+class App extends Component {
+    render(){
+        return(
+            <>
+                <div>
+                    <p>
+                        lalallfala
+                    </p>
+                </div>
+            </>
+        )
+    }
+}
+
+export default App;
